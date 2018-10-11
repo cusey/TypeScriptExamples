448f55b19b9bdf26a7a0bb5e421afeba0bab6165
John Cusey <jjcusey@gmail.com>
2018-10-11 12:27:25 -0400

    Update Variable Declarations.ts

--- Variable Declarations.ts
+++ Variable Declarations.ts
@@ -11,7 +11,8 @@ let backgroundColor = Color.Red;
 
 /*
 const declarations
-They are like let declarations but, as their name implies, their value cannot be changed once they are bound. In other words, they have the same scoping rules as let, but you can’t re-assign to them.
+They are like let declarations but, as their name implies, their value cannot be changed once they are bound.
+In other words, they have the same scoping rules as let, but you can’t re-assign to them.
 */
 
 const g = 9;
@@ -19,7 +20,9 @@ g = 9
 
 /*
 Block-scoping
-When a variable is declared using let, it uses what some call lexical-scoping or block-scoping. Unlike variables declared with var whose scopes leak out to their containing function, block-scoped variables are not visible outside of their nearest containing block or for-loop.
+When a variable is declared using let, it uses what some call lexical-scoping or block-scoping. Unlike variables
+declared with var whose scopes leak out to their containing function, block-scoped variables are not visible outside 
+of their nearest containing block or for-loop.
 */
 
 function h(input: boolean) {
@@ -39,8 +42,9 @@ h(false);
 
 /*
 var declarations
-In this above example, 'o' captured the variable 'l' declared in 'k'. At any point that 'o' gets called, the value of 'l' will be tied to the value of 'l' in 'k'. 
-Even if 'o' is called once 'k' is done running, it will be able to access and modify 'l'.
+In this above example, 'o' captured the variable 'l' declared in 'k'. At any point that 'o' gets called, the value
+of 'l' will be tied to the value of 'l' in 'k'. Even if 'o' is called once 'k' is done running, it will be able to 
+access and modify 'l'.
 */
 
 function k() {
